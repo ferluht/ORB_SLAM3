292d7591d11f94906e4f027a12335846af963d4d
ferluht <ilya.belikov@phystech.edu>
2020-12-19 10:18:11 +0300

    queue listener

--- Examples/RGBD-AMQP/rgbd_amqp.cc
+++ Examples/RGBD-AMQP/rgbd_amqp.cc
@@ -1,33 +1,51 @@
-
 #include <stdint.h>
+#include <ctype.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+
+#include<opencv2/core/core.hpp>
+
 #include <amqp.h>
 #include <amqp_tcp_socket.h>
+#include <amqp_framing.h>
 
 #include <assert.h>
 
-#include <opencv2/core/core.hpp>
+#include <System.h>
 
-#include "utils.h"
+#define SUMMARY_EVERY_US 1000000
+
+void die(const char *fmt, ...);
+void die_on_error(int x, char const *context);
+void die_on_amqp_error(amqp_rpc_reply_t x, char const *context);
+
+void amqp_dump(void const *buffer, size_t len);
 
 int main(int argc, char const *const *argv) {
   char const *hostname;
   int port, status;
-  char const *queuename = "images";
+  char const *bindingkey;
   amqp_socket_t *socket = NULL;
   amqp_connection_state_t conn;
 
+  char const * images_queue = "images";
+  char const * poses_queue = "poses";
+
   if (argc < 5) {
-    fprintf(stderr, "Usage: rgbd_amqp path_to_vocabulary path_to_settings host port\n");
+    fprintf(stderr, "Usage: amqp_consumer host port path_to_vocabulary path_to_settings\n");
     return 1;
   }
 
-  hostname = argv[3];
-  port = atoi(argv[4]);
-  
+  hostname = argv[1];
+  port = atoi(argv[2]);
+
   conn = amqp_new_connection();
 
   socket = amqp_tcp_socket_new(conn);
@@ -41,16 +59,24 @@ int main(int argc, char const *const *argv) {
   }
 
   die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN,
-                               "guest", "guest"),
+                               "rabbitmq", "rabbitmq"),
                     "Logging in");
   amqp_channel_open(conn, 1);
   die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
 
-  amqp_basic_consume(conn, 1, amqp_cstring_bytes(queuename), amqp_empty_bytes,
-                     0, 0, 0, amqp_empty_table);
+  // amqp_queue_declare(conn, 1, amqp_cstring_bytes(images_queue), 0, 0, 0, 1, amqp_empty_table);
+
+  // amqp_queue_declare(conn, 1, amqp_cstring_bytes(poses_queue), 0, 0, 0, 1, amqp_empty_table);
+
+  amqp_basic_consume(conn, 1, amqp_cstring_bytes(images_queue), amqp_empty_bytes, 0, 1, 0,
+                     amqp_empty_table);
   die_on_amqp_error(amqp_get_rpc_reply(conn), "Consuming");
 
-  ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::RGBD,true);
+  printf("Consuming");
+
+  ORB_SLAM3::System SLAM(argv[3],argv[4],ORB_SLAM3::System::RGBD,true);
+
+  cv::Mat imRGB, imD;
 
   for (;;) {
     amqp_rpc_reply_t res;
@@ -76,20 +102,36 @@ int main(int argc, char const *const *argv) {
     }
     printf("----\n");
 
+    {
+      amqp_basic_properties_t props;
+      props._flags = AMQP_BASIC_CONTENT_TYPE_FLAG | AMQP_BASIC_DELIVERY_MODE_FLAG;
+      props.content_type = amqp_cstring_bytes("text/plain");
+      props.delivery_mode = 2; /* persistent delivery mode */
+      die_on_error(amqp_basic_publish(conn, 1, amqp_cstring_bytes(""),
+                                      amqp_cstring_bytes(poses_queue), 0, 0,
+                                      &props, amqp_cstring_bytes("pose")),
+                   "Publishing");
+    }
+
     amqp_dump(envelope.message.body.bytes, envelope.message.body.len);
 
-    amqp_destroy_envelope(&envelope);
+    unsigned char *buffer = (unsigned char *)envelope.message.body.bytes;
+    int bufferLength = envelope.message.body.len;
 
-    cv::Mat imRGB, imD;
+    imRGB = cv::imdecode(cv::Mat(1, bufferLength, CV_8UC1, buffer), CV_LOAD_IMAGE_UNCHANGED);
 
-
+    imD = cv::imdecode(cv::Mat(1, bufferLength, CV_8UC1, buffer), CV_LOAD_IMAGE_UNCHANGED);
 
     double tframe = 0;
 
     SLAM.TrackRGBD(imRGB,imD,tframe);
+
+    amqp_destroy_envelope(&envelope);
   }
 
-  SLAM.Shutdown();
+  // run(conn);
+
+  // amqp_bytes_free(queuename);
 
   die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS),
                     "Closing channel");
@@ -98,4 +140,151 @@ int main(int argc, char const *const *argv) {
   die_on_error(amqp_destroy_connection(conn), "Ending connection");
 
   return 0;
+}
+
+
+
+// Utils
+
+
+void die(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  va_end(ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+void die_on_error(int x, char const *context) {
+  if (x < 0) {
+    fprintf(stderr, "%s: %s\n", context, amqp_error_string2(x));
+    exit(1);
+  }
+}
+
+void die_on_amqp_error(amqp_rpc_reply_t x, char const *context) {
+  switch (x.reply_type) {
+    case AMQP_RESPONSE_NORMAL:
+      return;
+
+    case AMQP_RESPONSE_NONE:
+      fprintf(stderr, "%s: missing RPC reply type!\n", context);
+      break;
+
+    case AMQP_RESPONSE_LIBRARY_EXCEPTION:
+      fprintf(stderr, "%s: %s\n", context, amqp_error_string2(x.library_error));
+      break;
+
+    case AMQP_RESPONSE_SERVER_EXCEPTION:
+      switch (x.reply.id) {
+        case AMQP_CONNECTION_CLOSE_METHOD: {
+          amqp_connection_close_t *m =
+              (amqp_connection_close_t *)x.reply.decoded;
+          fprintf(stderr, "%s: server connection error %uh, message: %.*s\n",
+                  context, m->reply_code, (int)m->reply_text.len,
+                  (char *)m->reply_text.bytes);
+          break;
+        }
+        case AMQP_CHANNEL_CLOSE_METHOD: {
+          amqp_channel_close_t *m = (amqp_channel_close_t *)x.reply.decoded;
+          fprintf(stderr, "%s: server channel error %uh, message: %.*s\n",
+                  context, m->reply_code, (int)m->reply_text.len,
+                  (char *)m->reply_text.bytes);
+          break;
+        }
+        default:
+          fprintf(stderr, "%s: unknown server error, method id 0x%08X\n",
+                  context, x.reply.id);
+          break;
+      }
+      break;
+  }
+
+  exit(1);
+}
+
+static void dump_row(long count, int numinrow, int *chs) {
+  int i;
+
+  printf("%08lX:", count - numinrow);
+
+  if (numinrow > 0) {
+    for (i = 0; i < numinrow; i++) {
+      if (i == 8) {
+        printf(" :");
+      }
+      printf(" %02X", chs[i]);
+    }
+    for (i = numinrow; i < 16; i++) {
+      if (i == 8) {
+        printf(" :");
+      }
+      printf("   ");
+    }
+    printf("  ");
+    for (i = 0; i < numinrow; i++) {
+      if (isprint(chs[i])) {
+        printf("%c", chs[i]);
+      } else {
+        printf(".");
+      }
+    }
+  }
+  printf("\n");
+}
+
+static int rows_eq(int *a, int *b) {
+  int i;
+
+  for (i = 0; i < 16; i++)
+    if (a[i] != b[i]) {
+      return 0;
+    }
+
+  return 1;
+}
+
+void amqp_dump(void const *buffer, size_t len) {
+  unsigned char *buf = (unsigned char *)buffer;
+  long count = 0;
+  int numinrow = 0;
+  int chs[16];
+  int oldchs[16] = {0};
+  int showed_dots = 0;
+  size_t i;
+
+  for (i = 0; i < len; i++) {
+    int ch = buf[i];
+
+    if (numinrow == 16) {
+      int j;
+
+      if (rows_eq(oldchs, chs)) {
+        if (!showed_dots) {
+          showed_dots = 1;
+          printf(
+              "          .. .. .. .. .. .. .. .. : .. .. .. .. .. .. .. ..\n");
+        }
+      } else {
+        showed_dots = 0;
+        dump_row(count, numinrow, chs);
+      }
+
+      for (j = 0; j < 16; j++) {
+        oldchs[j] = chs[j];
+      }
+
+      numinrow = 0;
+    }
+
+    count++;
+    chs[numinrow++] = ch;
+  }
+
+  dump_row(count, numinrow, chs);
+
+  if (numinrow != 0) {
+    printf("%08lX:\n", count);
+  }
 }
